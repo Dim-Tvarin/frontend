2ee4877cb0d2e0813049c2e0478ee64b297e67a9
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-02-20 17:31:08 +0200

    initial build

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      components: "/src/components",
+      helpers: "/src/helpers",
+      pages: "/src/pages",
+      hooks: "/src/hooks",
+    },
+  },
+});
