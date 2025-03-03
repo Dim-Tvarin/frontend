56009bd1a1e805d6f398eb94f6db26a115350956
koseckaya <kosetsaya@gmail.com>
2025-03-03 22:12:22 +0200

    feat: styled links

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -6,32 +6,38 @@ import cabinet from '../../assets/cabinet.svg'
 export const Header = () => {
   return (
     <header className='h-[100px] bg-[#f7ebeb] flex items-center container'>
-        <nav>
-          <ul className='flex flex-row gap-[80px] items-center'>
-          <li>
-            <NavLink to="/" end>
-              <img src={logo} alt="logo" />
-            </NavLink></li>
-          <li>
-            <NavLink to="/find" end>
-              Знайти тваринку
-            </NavLink></li>
-          <li>
-            <NavLink to="/giveaway" end>
-              Віддати тваринку
-            </NavLink></li>
-          <li>
-            <NavLink to="/lookfor" end>
-              Доглянути за тваринкою
-            </NavLink></li>
-          <li>
-            <NavLink to="/favorite" end>
-              <img src={hart} alt="favorite pets" />
-            </NavLink></li>
-          <li>
-            <NavLink to="/register" end>
-               <img src={cabinet} alt="personal cabinet" />
-            </NavLink></li>
+        <nav className='w-full'>
+          <ul className='flex flex-row items-center'>
+            <li className='mr-auto'>
+              <NavLink to="/" end>
+                <img src={logo} alt="logo" />
+              </NavLink>
+            </li>
+            <li className='mr-[80px]'>
+              <NavLink to="/find" end>
+                Знайти тваринку
+              </NavLink>
+            </li>
+            <li className='mr-[80px]'>
+              <NavLink to="/giveaway" end>
+                Віддати тваринку
+              </NavLink>
+            </li>
+            <li className='mr-[80px]'>
+              <NavLink to="/lookfor" end>
+                Доглянути за тваринкою
+              </NavLink>
+            </li>
+            <li  className='ml-auto mr-[20px]'>
+              <NavLink to="/favorite" end>
+                <img src={hart} alt="favorite pets" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" end>
+                <img src={cabinet} alt="personal cabinet" />
+              </NavLink>
+            </li>
           </ul>
         </nav>
     </header>
