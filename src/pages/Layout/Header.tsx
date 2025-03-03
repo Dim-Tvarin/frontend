06cde8429afd2c64f7e8e45e4ffbb035aeffac98
koseckaya <kosetsaya@gmail.com>
2025-03-03 21:59:36 +0200

    feat: add header & add navigation

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
+import hart from '../../assets/hart.svg'
+import cabinet from '../../assets/cabinet.svg'
 
 export const Header = () => {
   return (
-    <header>Header
-      <div className="container">
-       <nav>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <NavLink to="/register" end>
-        Sign Up
-      </NavLink>
+    <header className='h-[100px] bg-[#f7ebeb] flex items-center container'>
+        <nav>
+          <ul className='flex flex-row gap-[80px] items-center'>
+          <li>
+            <NavLink to="/" end>
+              <img src={logo} alt="logo" />
+            </NavLink></li>
+          <li>
+            <NavLink to="/find" end>
+              Знайти тваринку
+            </NavLink></li>
+          <li>
+            <NavLink to="/giveaway" end>
+              Віддати тваринку
+            </NavLink></li>
+          <li>
+            <NavLink to="/lookfor" end>
+              Доглянути за тваринкою
+            </NavLink></li>
+          <li>
+            <NavLink to="/favorite" end>
+              <img src={hart} alt="favorite pets" />
+            </NavLink></li>
+          <li>
+            <NavLink to="/register" end>
+               <img src={cabinet} alt="personal cabinet" />
+            </NavLink></li>
+          </ul>
         </nav>
-      </div>
     </header>
   )
 }
