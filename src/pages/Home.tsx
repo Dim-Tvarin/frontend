159961aacd2d35f1ead93a40e62c67660f35dead
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-03-04 12:52:11 +0200

    add logaut btn to Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,7 @@
-import { CustomButton } from 'components/CustomButton';
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../redux/store';
-import { logoutThunk } from '../redux/users/usersOperations';
-
 export const Home = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const handleClick = () => {
-    dispatch(logoutThunk());
-  };
   return (
     <>
-      <div>Home - Текст домашньої сторінки marketplace</div>
-      <CustomButton
-        type="button"
-        styleType="defaultButton"
-        className="w-[30px]"
-        onClick={handleClick}
-      >
-        Вийти
-      </CustomButton>
+      <div>Home</div>
     </>
   );
 };
--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -2,8 +2,17 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import hart from '../../assets/hart.svg';
 import cabinet from '../../assets/cabinet.svg';
+import { CustomButton } from 'components/CustomButton';
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from '../../redux/store';
+import { logoutThunk } from '../../redux/users/usersOperations';
 
 export const Header = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const handleClick = () => {
+    dispatch(logoutThunk());
+  };
+
   return (
     <header className="h-[100px] bg-[#f7ebeb] flex items-center container">
       <nav className="w-full">
@@ -37,6 +46,16 @@ export const Header = () => {
             <NavLink to="/register" end>
               <img src={cabinet} alt="personal cabinet" />
             </NavLink>
+          </li>
+          <li>
+            <CustomButton
+              type="button"
+              styleType="defaultButton"
+              className="w-[100px] m-auto ml-[20px]"
+              onClick={handleClick}
+            >
+              Вийти
+            </CustomButton>
           </li>
         </ul>
       </nav>
