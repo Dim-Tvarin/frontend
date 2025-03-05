35f6e1a2c5655918695ba3e603505e0a35cd5281
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-03-05 17:26:18 +0200

    fix: UI pink boxes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,11 +14,15 @@ export const Home = () => {
             <Link to="/lookfor">Створити оголошення</Link>
           </CustomButton>
         </div>
-        <div className="h-[510px] w-[580px] bg-main-pink-l rounded-[30px]  border-l-[50px] border-t-[50px]"></div>
+        <div className="relative h-[557px] w-[631px] bg-main-pink-d rounded-[30px]">
+          <div className="absolute bottom-0 right-0 h-[507px] w-[581px] bg-main-pink-l rounded-[30px]"></div>
+        </div>
       </section>
 
       <section className="flex flex-row gap-[20px] mt-[134px]">
-        <div className="h-[475px] w-[580px] bg-main-pink-l rounded-[30px] border-r-[50px] border-t-[50px]"></div>
+        <div className="relative h-[525px] w-[630px] bg-main-pink-d rounded-[30px]">
+          <div className="absolute bottom-0 left-0 h-[475px] w-[580px] bg-main-pink-l rounded-[30px]"></div>
+        </div>
         <div className="flex flex-col gap-[24px] justify-start w-2/4 items-start">
           <h3 className="mb-[67px] text-xxl">Трішки про нас</h3>
           <p className="text-l">Ми любимо тварин тому...</p>
