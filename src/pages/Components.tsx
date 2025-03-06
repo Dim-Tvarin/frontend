ac1c226008cf0704ddeb939ab7e6e7321a51de5e
koseckaya <kosetsaya@gmail.com>
2025-03-06 13:26:00 +0200

    fix: ts conflict

--- src/pages/Components.tsx
+++ src/pages/Components.tsx
@@ -3,10 +3,13 @@ import { CustomButton } from 'components/CustomButton';
 
  const Components = () => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-72">
             Button
-           <CustomButton type="submit" styleType="defaultButton">
-                Увійти
+           <CustomButton styleType="defaultButton" variant="default" >
+                Button
+            </CustomButton>
+            <CustomButton styleType="defaultButton" variant="link" >
+                Button
             </CustomButton>
         </div>
     );
