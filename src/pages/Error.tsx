2999d89e48e2f9d24f3a52577bfc1f96b5a49e6a
koseckaya <kosetsaya@gmail.com>
2025-03-19 16:53:02 +0200

    feat: add Error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom';
-
 const Error = () => {
   return (
-    <>
-      <h1>404 not found</h1>
-      <Link to="/">Go home</Link>
-    </>
+    <div className='h-[calc(100vh-259px)] flex flex-col justify-center items-center bg-[url(src/assets/404.jpg)] bg-cover bg-center '>
+      <h1 className='text-white text-3xl mb-12'>Ой!</h1>
+      <h2 className='text-black text-40'>Щось пішло не так...</h2>
+      <p className='text-black text-xl'>Спробуйте перезавантажити сторінку</p>
+    </div>
   );
 };
 export default Error;
