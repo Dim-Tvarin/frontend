b58b375c4bad45a20d87488bbb5ff25a6bf7f5c8
koseckaya <kosetsaya@gmail.com>
2025-03-19 19:23:28 +0200

    add bigger bg picture

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,6 @@
 const Error = () => {
   return (
-    <div className='h-[calc(100vh-259px)] flex flex-col justify-center items-center bg-[url(src/assets/404.jpg)] bg-cover bg-center '>
+    <div className='h-[calc(100vh-259px)] flex flex-col justify-center items-center bg-[url(src/assets/404@2x.jpg)] bg-cover bg-center '>
       <h1 className='text-white text-3xl mb-12'>Ой!</h1>
       <h2 className='text-black text-40'>Щось пішло не так...</h2>
       <p className='text-black text-xl'>Спробуйте перезавантажити сторінку</p>
