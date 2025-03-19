8afde38b90b5eedfcf3679f8b768e04481ccec16
koseckaya <kosetsaya@gmail.com>
2025-03-19 15:05:39 +0200

    fix: change text in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
-    <div className="container">
+    <div className="container text-default-btn">
       <section className="flex flex-row gap-[20px] mt-[60px]">
         <div className="flex flex-col gap-[24px] justify-center w-2/4 items-center">
           <p className="text-xl px-[20px]">
-            Знайдіть свого ідеального домашнього улюбленця, або допоможіть
-            безпритульним тваринкам знайти свій дім
+           Оберіть для себе ідеального домашнього улюбленця, або допоможіть безпритульним тваринам знайти свій дім
           </p>
-          <CustomButton className="w-[236px] bg-default-btn rounded-[20px]">
+          <CustomButton className="w-[236px] h-[44px] bg-default-btn rounded-[20px]">
             <Link to="/lookfor">Створити оголошення</Link>
           </CustomButton>
         </div>
@@ -24,9 +23,10 @@ export const Home = () => {
         <div className="relative h-[525px] w-[630px] bg-main-pink-d rounded-[30px]">
           <div className="absolute bottom-0 left-0 h-[475px] w-[580px] bg-main-pink-l rounded-[30px]"></div>
         </div>
-        <div className="flex flex-col gap-[24px] justify-start w-2/4 items-start">
-          <h3 className="mb-[67px] text-xxl">Трішки про нас</h3>
-          <p className="text-l">Ми любимо тварин тому...</p>
+        <div className="flex flex-col gap-[24px] justify-start w-2/4 items-start text-start">
+          <h3 className="mb-8 text-xl font-semibold">Чому саме ми?</h3>
+          <p className="text-m font-normal">Ми та платформа, що об&#39;єднує людей, яким небайдужа доля тварин! Ми створили цей сервіс для того, щоб повертати загублених улюбленців, знаходити новий дім для тварин, а також забезпечувати безпечний та відповідальний пошук чотирилапих друзів.</p>
+          <p className="text-m font-normal">Ми прагнемо створити суспільство відповідальних власників, де кожен чотирилапий друг отримає шанс на щасливе життя. Долучайтеся до нас, разом ми зможемо більше!</p>
         </div>
       </section>
     </div>
