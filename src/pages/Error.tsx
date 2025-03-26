8b09f948119298ddca3a74d22f1bae9f3dca432a
koseckaya <kosetsaya@gmail.com>
2025-03-26 23:40:23 +0200

    fix: Error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,11 @@
 const Error = () => {
   return (
-    <div className='h-[calc(100vh-259px)] flex flex-col justify-center items-center bg-[url(src/assets/404@2x.jpg)] bg-cover bg-center '>
-      <h1 className='text-white text-3xl mb-12'>Ой!</h1>
-      <h2 className='text-black text-40'>Щось пішло не так...</h2>
-      <p className='text-black text-xl'>Спробуйте перезавантажити сторінку</p>
+    <div className="h-[calc(100vh-259px)] flex flex-col justify-center items-center bg-[url(public/assets/404@2x.jpg)] bg-cover bg-center ">
+      <h1 className="text-white text-[150px] mb-12">Ой!</h1>
+      <h2 className="text-black text-[40px]">Щось пішло не так...</h2>
+      <p className="text-black text-[32px]">
+        Спробуйте перезавантажити сторінку
+      </p>
     </div>
   );
 };
