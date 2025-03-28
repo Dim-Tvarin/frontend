2db2859553de28781a16f2e136af899e3ff7ad60
koseckaya <kosetsaya@gmail.com>
2025-03-28 13:18:52 +0200

    fix: img path

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,6 @@
 const Error = () => {
   return (
-    <div className="h-[calc(100vh-253px)] flex flex-col justify-center items-center bg-[url(public/assets/404@2x.jpg)] bg-cover bg-center ">
+    <div className="h-[calc(100vh-253px)] flex flex-col justify-center items-center bg-[url(../../public/assets/404@2x.jpg)] bg-cover bg-center ">
       <h1 className="text-white text-[150px] mb-12">Ой!</h1>
       <h2 className="text-black text-[40px]">Щось пішло не так...</h2>
       <p className="text-black text-[32px]">
