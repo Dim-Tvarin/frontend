3acf3ca748dce1ff321024704616b9417bbaa20c
koseckaya <kosetsaya@gmail.com>
2025-04-02 20:51:40 +0300

    create branch week-10

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ import track from '../../public/track.png';
 
 export const Home = () => {
   const navigate = useNavigate();
-
+  console.log('week-10');
   return (
     <div className="container text-default-btn relative">
       <section className="flex flex-row gap-[20px] mt-[100px]">
