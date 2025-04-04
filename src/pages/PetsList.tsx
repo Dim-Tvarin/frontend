13a04b5930be2d7bf67c0de6d2e38b7f3872a006
MilaKoseckaya <kosetsaya@gmail.com>
2025-04-04 17:27:58 +0300

    feat: add header section

--- src/pages/PetsList.tsx
+++ src/pages/PetsList.tsx
@@ -1,9 +1,21 @@
+import { FiFilter } from "react-icons/fi";
+import { CustomButton } from "components/CustomButton";
 
 
 const PetsList = () => {
   return (
-    <div>
-      PetsList1
+    <div className="container">
+      <div className="flex justify-between mt-100 mb-50">
+        <h1 className="text-[32px]">Всі тварини</h1>
+        <CustomButton
+          type="button"
+          styleType="defaultButton"
+          className="w-[129px] m-0"
+        >
+          <FiFilter size={18} />
+          <span className="text-lg">Фільтр</span>
+        </CustomButton>
+      </div>
     </div>
   );
 }
