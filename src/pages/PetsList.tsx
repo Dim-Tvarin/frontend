1a250f7c498a23f1b065a90375df75403a995a4f
MilaKoseckaya <kosetsaya@gmail.com>
2025-04-05 18:49:55 +0300

    feat: add sceleton

--- src/pages/PetsList.tsx
+++ src/pages/PetsList.tsx
@@ -2,14 +2,13 @@ import { FiFilter } from "react-icons/fi";
 import { CustomButton } from "components/CustomButton";
 import { useGetAnimalsQuery } from "src/redux/animals/animalsApi";
 import AnimalCard from "components/AnimalCard";
+import { PetsListSkeleton } from "components/sceletons/PetsListSkeleton";
 
 
 const PetsList = () => {
-
   const { data, error, isLoading } = useGetAnimalsQuery({})
 
   console.log(data, error, isLoading)
-
   return (
     <div className="container">
       <div className="flex justify-between mt-100 mb-50">
@@ -23,9 +22,22 @@ const PetsList = () => {
           <span className="text-lg">Фільтр</span>
         </CustomButton>
       </div>
-      <div className="grid grid-cols-4 gap-20">
-        {data?.animals.map(item => <AnimalCard key={item.id} id={item.id} name={item.animalName} gender={item.gender} age={item.age} photoSrc={item.animalImages[0]}/>)}
-      </div>
+      {isLoading ? (
+        <PetsListSkeleton />
+      ) : (
+        <div className="grid grid-cols-4 gap-20">
+          {data?.animals.map(item => (
+            <AnimalCard
+              key={item.id}
+              id={item.id}
+              name={item.animalName}
+              gender={item.gender}
+              age={item.age}
+              photoSrc={item.animalImages[0]}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
