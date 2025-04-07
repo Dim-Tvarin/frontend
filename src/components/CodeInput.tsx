d59a55e21ae62f6fb247ceb11e975bb4f3b9f670
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-04-08 02:24:49 +0300

    feat: create CodeInput

---
+++ src/components/CodeInput.tsx
@@ -0,0 +1,54 @@
+import type { FC } from 'react';
+import { cn } from './lib/utils';
+import { CustomLabel } from './CustomLabel';
+import FormError from './FormError';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from './components/ui/input-otp';
+
+interface CodeInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  id: string;
+  error?: string;
+  labelSize?: string;
+  labelClass?: string;
+  length?: number;
+}
+
+export const CodeInput: FC<CodeInputProps> = ({
+  id,
+  label,
+  className,
+  labelClass,
+  labelSize = 'xs',
+  error,
+  length = 6
+}) => {
+  return (
+    <div className="w-full">
+      {label && (
+        <CustomLabel htmlFor={id} labelSize={labelSize} labelClass={labelClass}>
+          {label}
+        </CustomLabel>
+      )}
+     
+        <InputOTP maxLength={length}>
+          <InputOTPGroup className='flex gap-10'>
+            {[...Array(length)].map((_, i) => (
+              <InputOTPSlot
+                key={i}
+                index={i}
+                className={cn(
+                  'border border-input-border rounded-[8px] w-50 h-50 placeholder:text-input-border',
+                  { 'border-error-input': error },
+                  className
+                )}
+              />
+            ))}
+          </InputOTPGroup>
+        </InputOTP>
+ 
+      {error && <FormError error={error} />}
+    </div>
+  );
+};
+
+
