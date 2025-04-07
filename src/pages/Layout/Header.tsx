87d8bebabc0c714fb1882f87f417da906507864f
MilaKoseckaya <kosetsaya@gmail.com>
2025-04-07 17:56:31 +0300

    feat: refactor header

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -20,67 +20,56 @@ export const Header = () => {
 
   return (
     <header className="h-100 bg-header flex items-center">
-      <nav className="container">
-        <ul className="flex flex-row items-center text-s">
-          <li className="mr-auto">
-            <NavLink className="flex flex-col items-center" to="/" end>
-              <img src={logo} alt="logo" className="w-46 h-46" />
-              <span className="text-xxs">Dim Tvaryn</span>
-            </NavLink>
-          </li>
-          <li className="mr-56">
-            <NavLink to="/find" end>
-              Знайти тварину
-            </NavLink>
-          </li>
-          <li className="mr-56">
-            <NavLink to="/announcement" end>
-              Віддати в добрі руки
-            </NavLink>
-          </li>
-          <li className="mr-56">
-            <NavLink to="/lookfor" end>
-              Догляд за твариною
-            </NavLink>
-          </li>
+      <div className="container flex items-center justify-between text-medium tracking-1" >
+        <NavLink className="flex flex-col items-center mr-auto" to="/" end>
+          <img src={logo} alt="logo" className="w-46 h-46" />
+          <span className="text-sm">Dim Tvaryn</span>
+        </NavLink>
 
-          <li className="ml-auto mr-20">
-            <NavLink to="/favorite" end>
-              <img src={search} alt="search" />
-            </NavLink>
-          </li>
-          <li className="mr-20">
-            <NavLink to="/favorite" end>
-              <FaRegHeart size="30px" />
-            </NavLink>
-          </li>
-          <li>
-            {isLoggedIn ? (
-              <>
-                <span className="text-black text-s">{username}</span>
-                <CustomButton
-                  type="button"
-                  styleType="redButton"
-                  className="w-[100px] m-auto ml-[20px]"
-                  onClick={handleClick}
-                >
-                  Вийти
-                </CustomButton>
-              </>
-            ) : (
+        <nav className="flex flex-row items-center text-lg gap-56">
+          <NavLink to="/find" end>
+            Знайти тварину
+          </NavLink>
+          <NavLink to="/announcement" end>
+            Віддати в добрі руки
+          </NavLink>
+          <NavLink to="/lookfor" end>
+            Догляд за твариною
+          </NavLink>
+        </nav>
+        <div className="flex items-center gap-30 ml-100">
+          <NavLink to="/favorite" end>
+            <img src={search} alt="search" />
+          </NavLink>
+
+          <NavLink to="/favorite" end>
+            <FaRegHeart size="30px" />
+          </NavLink>
+          {isLoggedIn ? (
+            <>
+              <span className="text-black text-s">{username}</span>
               <CustomButton
-                onClick={() => dispatch(openDialog('login'))}
                 type="button"
-                styleType="defaultButton"
-                className="w-100 m-auto"
+                styleType="redButton"
+                className="w-[100px] m-auto ml-[20px]"
+                onClick={handleClick}
               >
-                <CabinetSVG />
-                <span>Вхід</span>
+                Вийти
               </CustomButton>
-            )}
-          </li>
-        </ul>
-      </nav>
+            </>
+          ) : (
+            <CustomButton
+              onClick={() => dispatch(openDialog('login'))}
+              type="button"
+              styleType="defaultButton"
+              className="w-100 m-auto"
+            >
+              <CabinetSVG />
+              <span>Вхід</span>
+            </CustomButton>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
