ab275b01e035865275119610e203ab5f4242c608
MilaKoseckaya <kosetsaya@gmail.com>
2025-04-08 15:16:09 +0300

    feat: add query

--- src/pages/PetPage.tsx
+++ src/pages/PetPage.tsx
@@ -1,7 +1,36 @@
+import { useNavigate, useParams } from "react-router";
+import { useGetAnimalByIdQuery } from "src/redux/animals/animalsApi";
+
 const PetPage = () => {
+ const navigate = useNavigate();
+ const {id} = useParams<{ id: string }>()
+  if (!id) {
+    alert("Щось пішло не так")
+    navigate('/allpets')
+    return
+  } 
+  const { data, error, isLoading } = useGetAnimalByIdQuery(id)
+
+
+  if (isLoading ) {
+    return null
+  } 
+  const { animal } = data || {}
+
+  console.log('', id, data, error);
   return (
-    <div>
-      Pet card
+    <div className="flex gap-20">
+      <div className="w-1/2">Pic</div>
+      <div className="w-1/2 flex flex-col text-5xl font-medium">
+        <h2>{animal?.animalName}</h2>
+        <div className="grid grid-col-2 gap-x-130 text-xl">
+          <p className="font-bold">Статус:</p>
+          <p className="text-base">{animal?.status}</p>
+          <p className="font-bold">Вид:</p>
+          <p className="text-base">{animal?.animalType}</p>
+
+        </div>
+      </div>
     </div>
   );
 }
