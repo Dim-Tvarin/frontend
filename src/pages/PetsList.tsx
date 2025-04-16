9c9f9641f7a76db53ba465d69049b7cdb0394cd5
MilaKoseckaya <kosetsaya@gmail.com>
2025-04-16 19:35:53 +0300

    change page limit

--- src/pages/PetsList.tsx
+++ src/pages/PetsList.tsx
@@ -13,7 +13,7 @@ import BreedSelect from "components/BreedSelect";
 import { CitySelect } from "components/CitySelect";
 import { Controller, useForm } from "react-hook-form";
 
-const limit = 12
+const limit = 4
 
 interface FilterFormValues {
   animalType: AnimalTypeEnum | undefined;
