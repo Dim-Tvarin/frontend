e47b482eadfd35f02360de908a1a21e00dc0a873
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-05 22:22:36 +0300

    feat: add mobile menu

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -14,16 +14,22 @@ import {
 } from 'components/components/ui/avatar';
 import fallbackIcon from '../../assets/avatar-icon.png';
 import { cn } from 'components/lib/utils';
+import { useState } from 'react';
+import { RxCross2 } from 'react-icons/rx';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const user = useSelector(selectUser);
 
+  console.log('isMobileMenuOpen', isMobileMenuOpen);
   return (
     <header className="xs:h-80 h-100 bg-header flex items-center">
-      <nav className="xs:hidden container flex items-center justify-between">
+      {/* DESKTOP NAV */}
+      <nav className="xs:hidden container lg:flex items-center justify-between">
         <NavLink className="flex flex-col items-center gap-2" to="/" end>
           <img src={logo} alt="logo" className="w-46 h-46" />
           <span className="text-sm hover:text-default-btn transition:all duration-300">
@@ -122,6 +128,76 @@ export const Header = () => {
           )}
         </div>
       </nav>
+
+      {/* MOBILE HEADER */}
+      <div className="container xs:flex hidden items-center justify-between w-full px-4 lg:hidden">
+        <button
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+          className="focus:outline-none"
+        >
+          {isMobileMenuOpen ? (
+            <RxCross2 size={24} />
+          ) : (
+            <RxHamburgerMenu size={24} />
+          )}
+        </button>
+
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <img src={logo} alt="logo" className="w-40 h-40" />
+          <span className="text-sm">Dim Tvaryn</span>
+        </NavLink>
+
+        <div className="flex gap-4">
+          <button onClick={() => navigate('/favorite')}>
+            <GoHeart size={24} />
+          </button>
+          <button onClick={() => navigate(isLoggedIn ? '/profile' : '/login')}>
+            {isLoggedIn ? (
+              <div className="flex flex-col items-center max-h-[54px]">
+                <Avatar
+                  className="size-9 text-[10px] p-0 m-4"
+                  onClick={() => navigate('/profile')}
+                >
+                  <AvatarImage
+                    src={user.avatarURL}
+                    alt={`Аватар ${user.name}`}
+                  />
+                  <AvatarFallback>
+                    <img
+                      src={fallbackIcon}
+                      alt={`Аватар ${user.name}`}
+                      className="w-full h-full object-cover rounded-full m-0 p-0"
+                    />
+                  </AvatarFallback>
+                </Avatar>
+                <span className="text-[10px] leading-[140%] tracking-[0.01em] m-0">
+                  {user.name}
+                </span>
+              </div>
+            ) : (
+              <CabinetSVG color="black" />
+            )}
+          </button>
+        </div>
+      </div>
+
+      {/* MOBILE MENU PANEL */}
+      {isMobileMenuOpen && (
+        <div className="absolute top-80 left-0 w-full bg-[#f0f4fa] p-4 shadow-md z-50 rounded-b-lg xs:flex flex-col gap-16 text-lg font-medium text-default-btn">
+          <NavLink to="/profile" onClick={() => setMobileMenuOpen(false)}>
+            Особистий кабінет
+          </NavLink>
+          <NavLink to="/allpets" onClick={() => setMobileMenuOpen(false)}>
+            Знайти тварину
+          </NavLink>
+          <NavLink to="/announcement" onClick={() => setMobileMenuOpen(false)}>
+            Віддати в добрі руки
+          </NavLink>
+          <NavLink to="/lookfor" onClick={() => setMobileMenuOpen(false)}>
+            Догляд за твариною
+          </NavLink>
+        </div>
+      )}
     </header>
   );
 };
