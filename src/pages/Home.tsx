1a79439eba527fc7a8f5dd3c22898b6c91d12c60
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-05 20:47:00 +0300

    feat add styles for tablet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ export const Home = () => {
           <img src={track} alt="track" />
         </div>
         <div className="sm:w-full order-2 md:order-1 flex flex-col gap-16 lg:gap-[24px] justify-center md:w-2/4 items-center z-10">
-          <p className="text-base md:text-xl lg:text-[32px] px-[35px] md:px-[20px] font-normal z-10">
+          <p className="text-base md:text-xl lg:text-[32px] px-[35px] md:px-0 font-normal z-10">
             Оберіть для себе ідеального домашнього улюбленця, або допоможіть
             безпритульним тваринам знайти свій дім
           </p>
@@ -45,8 +45,8 @@ export const Home = () => {
             <img src={track} className="w-[300px] h-[320px]" alt="track" />
           </div>
         </div>
-        <div className="h-[202px] w-[328px] order-1 md:order-2 relative lg:h-[500px] lg:w-[630px] bg-orange rounded-[30px] overflow-hidden">
-          <div className="h-[186px] w-[312px] absolute bottom-0 right-0 lg:h-[468px] lg:w-[600px] rounded-[30px] z-10 overflow-hidden">
+        <div className="h-[202px] w-[328px] order-1 md:order-2 relative md:h-[213px] md:w-[294px] lg:h-[500px] lg:w-[630px] bg-orange rounded-[30px] overflow-hidden">
+          <div className="h-[186px] w-[312px] absolute bottom-0 right-0 md:h-[198px] md:w-[278px] lg:h-[468px] lg:w-[600px] rounded-[30px] z-10 overflow-hidden">
             <ResponsiveImage
               urlMax={homeDogMax}
               urlMin={homeDogMin}
@@ -59,9 +59,9 @@ export const Home = () => {
         </div>
       </section>
 
-      <section className="flex flex-col items-center md:flex-row gap-[20px] mt-32 lg:mt-100p mb-32 md:mb-50 lg:mb-100">
-        <div className="h-[202px] w-[328px] relative lg:h-[500px] lg:w-[630px] bg-orange rounded-[30px] overflow-hidden z-10">
-          <div className="h-[186px] w-[312px] absolute bottom-0 left-0 lg:h-[468px] lg:w-[600px]  rounded-[30px] overflow-hidden">
+      <section className="flex flex-col items-center md:flex-row gap-[20px] mt-32 md:mt-80 lg:mt-100p mb-32 md:mb-80 lg:mb-100">
+        <div className="relative h-[202px] w-[293px] md:h-[315px] md:w-[293px] lg:h-[500px] lg:w-[630px] bg-orange rounded-[30px] overflow-hidden z-10">
+          <div className="absolute bottom-0 left-0 h-[186px] w-[312px] md:h-[303px] md:w-[277px] lg:h-[468px] lg:w-[600px]  rounded-[30px] overflow-hidden">
             <ResponsiveImage
               urlMax={homeGirlDogMax}
               urlMin={homeGirlDogMin}
@@ -95,7 +95,7 @@ export const Home = () => {
         </h3>
         <AnimalsCarousel />
         <CustomButton
-          className="w-[236px] h-[44px] bg-default-btn rounded-[20px] z-10 flex gap-10"
+          className="w-[236px] h-[44px] bg-default-btn rounded-[20px] z-10 flex gap-10 mt-0 md:mt-16"
           onClick={() => navigate('/allpets')}
         >
           Переглянути всіх
