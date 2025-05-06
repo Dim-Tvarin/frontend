9e6a001daf5c00e251e214486b68721397b81896
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-06 17:33:14 +0300

    fix: carousel items

--- src/components/AnimalsCarousel.tsx
+++ src/components/AnimalsCarousel.tsx
@@ -30,7 +30,7 @@ const AnimalsCarousel = () => {
           data?.animals.map(item => (
             <CarouselItem
               key={item.id}
-              className="basis-[66.6%] md:basis-[50%] lg:basis-[37%] xl:basis-[29%] 2xl:basis-[25%] pl-16"
+              className="basis-[66.6%] md:basis-[50%] lg:basis-[37%] xl:basis-[29%] 2xl:basis-[25%] ml-16 pl-16"
             >
               <AnimalCard
                 key={item.id}
