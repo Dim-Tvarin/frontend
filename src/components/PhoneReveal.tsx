40a6a6233cf69215bd574a66ad61265590e38e4a
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-06 18:05:29 +0300

    feat: add text data of animal

--- src/components/PhoneReveal.tsx
+++ src/components/PhoneReveal.tsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export const PhoneReveal = ({ phone, className }: { phone: string; className?: string;}) => {
-  const [isRevealed, setIsRevealed] = useState(false)
+export const PhoneReveal = ({
+  phone,
+  className,
+}: {
+  phone: string;
+  className?: string;
+}) => {
+  const [isRevealed, setIsRevealed] = useState(false);
 
-  const maskedPhone = phone.replace(/(\+380)(\d{2})\d{5}\d{2}/, '$1ХХХХХХХ')
+  const maskedPhone = phone.replace(/(\+380)(\d{2})\d{5}\d{2}/, '$1ХХХХХХХ');
 
   return (
-    <div className={`${className} flex items-center gap-50`}>
-      <span className="text-xl font-medium">
-        {isRevealed ? phone : maskedPhone}
-      </span>
+    <div
+      className={`${className} flex items-center lg:gap-50 text-lg flex-col lg:flex-row`}
+    >
+      <span className=" font-medium">{isRevealed ? phone : maskedPhone}</span>
       {!isRevealed && (
         <button
           onClick={() => setIsRevealed(true)}
-          className="text-xl text-link hover:text-blue-600 transition"
+          className="text-link hover:text-blue-600 transition"
         >
           Показати номер
         </button>
       )}
     </div>
-  )
-}
+  );
+};
