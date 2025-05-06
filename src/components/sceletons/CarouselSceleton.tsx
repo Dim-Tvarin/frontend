c744cd5766d9e9985aab8cdc72e89940f5fa0b7b
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-06 21:18:08 +0300

    feat: responsive CarouselSceleton

--- src/components/sceletons/CarouselSceleton.tsx
+++ src/components/sceletons/CarouselSceleton.tsx
@@ -1,13 +1,33 @@
-import { Skeleton } from "components/components/ui/skeleton";
+import { Skeleton } from 'components/components/ui/skeleton';
+import { useEffect, useState } from 'react';
 
 export const CarouselSceleton = () => {
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    const updateCount = () => {
+      const width = window.innerWidth;
+      if (width < 640) setCount(1);
+      else if (width < 1024) setCount(2);
+      else if (width < 1360) setCount(3);
+      else setCount(4);
+    };
+
+    updateCount();
+    window.addEventListener('resize', updateCount);
+    return () => window.removeEventListener('resize', updateCount);
+  }, []);
+
   return (
-    <div className="flex gap-20">
-     {Array.from({ length: 4 }).map((_, i) => (
-        <div key={i} className="w-[305px] h-[400px] rounded-4xl overflow-hidden shadow-lg bg-header/20 animate-pulse">
+    <div className="flex gap-5 md:gap-20">
+      {Array.from({ length: count }).map((_, i) => (
+        <div
+          key={i}
+          className="w-[305px] h-[400px] rounded-4xl overflow-hidden shadow-lg bg-header/20 animate-pulse"
+        >
           <Skeleton className="w-full h-2/3 " />
         </div>
       ))}
     </div>
   );
-}
+};
