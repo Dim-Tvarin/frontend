6d1e91242c9576ad065b6e6dff5b0d66e09364f8
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-05-08 19:19:16 +0300

    fix: add container

--- src/pages/Auth/Registration.tsx
+++ src/pages/Auth/Registration.tsx
@@ -7,9 +7,9 @@ import dogWithRaisedPawMax from '../../assets/dog-with-raised-paw@2x.png';
 
 const Registration = () => {
   return (
-     <div className="relative w-full h-full">
+    <div className="container relative h-full">
       <div className="absolute top-[13px] right-[80px] w-[771px] h-[1078px] bg-[url('./src/assets/backgrounds/bg-paws-register.png')] bg-contain bg-no-repeat " />
-      <div className="grid grid-cols-2 gap-20 leading-[140%] tracking-[0.01em] p-80 pt-100 relative ">
+      <div className="grid grid-cols-2 gap-20 leading-[140%] tracking-[0.01em] pt-100 relative ">
         <div>
           <h2 className="text-[32px] mb-32">Ласкаво просимо</h2>
           <p className="text-2xl text-left mb-50">
