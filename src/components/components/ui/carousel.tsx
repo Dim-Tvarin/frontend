c857316b46dbbbfd7f5e2d993e50d69b112488e3
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-13 14:24:57 +0300

    fix: carousel styles

--- src/components/components/ui/carousel.tsx
+++ src/components/components/ui/carousel.tsx
@@ -184,7 +184,7 @@ function CarouselPrevious({
       variant={variant}
       size={size}
       className={cn(
-        'absolute size-8 rounded-full w-40 h-40 border-2 border-default-btn focus:outline-none',
+        'absolute size-8 rounded-full w-40 h-40 border-2 border-default-btn',
         orientation === 'horizontal'
           ? 'top-1/2 -left-56 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -214,7 +214,7 @@ function CarouselNext({
       variant={variant}
       size={size}
       className={cn(
-        'absolute size-8 rounded-full w-40 h-40 border-2 border-default-btn focus:outline-none',
+        'absolute size-8 rounded-full w-40 h-40 border-2 border-default-btn',
         orientation === 'horizontal'
           ? 'top-1/2 -right-56 -translate-y-1/2'
           : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
