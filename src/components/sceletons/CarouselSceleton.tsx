f78b6b15d5eb8e5d382c31f06923a48cc747dc7b
MilaKoseckaya <kosetsaya@gmail.com>
2025-05-19 18:57:36 +0300

    feat: add resize to carousel

--- src/components/sceletons/CarouselSceleton.tsx
+++ src/components/sceletons/CarouselSceleton.tsx
@@ -1,31 +1,31 @@
+import { useWindowSize } from '@uidotdev/usehooks';
 import { Skeleton } from 'components/components/ui/skeleton';
 import { useEffect, useState } from 'react';
 
 export const CarouselSceleton = () => {
   const [count, setCount] = useState(1);
+  const windowSize = useWindowSize();
 
   useEffect(() => {
-    const updateCount = () => {
-      const width = window.innerWidth;
-      if (width < 640) setCount(1);
-      else if (width < 1024) setCount(2);
-      else if (width < 1360) setCount(3);
-      else setCount(4);
-    };
-
-    updateCount();
-    window.addEventListener('resize', updateCount);
-    return () => window.removeEventListener('resize', updateCount);
-  }, []);
+    if (windowSize.width) {
+      if (windowSize.width < 640) {
+        setCount(1);
+      } else if (windowSize.width < 1024) {
+        setCount(2);
+      } else if (windowSize.width < 1360) {
+        setCount(3);
+      } else setCount(4);
+    }
+  }, [windowSize]);
 
   return (
     <div className="flex gap-5 md:gap-20">
       {Array.from({ length: count }).map((_, i) => (
         <div
           key={i}
-          className="w-[305px] h-[400px] rounded-4xl overflow-hidden shadow-lg bg-header/20 animate-pulse"
+          className="bg-header/20 shadow-lg rounded-4xl w-[305px] h-[400px] overflow-hidden animate-pulse"
         >
-          <Skeleton className="w-full h-2/3 " />
+          <Skeleton className="w-full h-2/3" />
         </div>
       ))}
     </div>
