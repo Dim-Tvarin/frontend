41141d74762dae81e2a593aa45e4ea12562effff
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-05-20 22:13:52 +0300

    feat: add registration mobile adaptive

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -62,17 +62,19 @@ const RegistrationForm: React.FC = () => {
   ];
 
   return (
-    <div className=" flex flex-col max-w-[630px]">
-      <h1 className="mb-[41px] text-[32px] text-black">Реєстрація акаунту</h1>
+    <div className=" flex flex-col xs:px-16 sm:w-full lg:max-w-[630px]">
+      <h1 className="text-default-btn xs:mb-[16px] xs:text-[18px] lg:mb-[41px] lg:text-[32px]">
+        Реєстрація акаунту
+      </h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col text-left gap-32"
+        className="flex flex-col text-left xs:gap-16 lg:gap-32"
       >
         <InputField
           label="Ім’я або назва організації"
           placeholder="Введіть ваше імʼя"
-          className="w-[630px] h-40"
-          labelClass="mb-16 text-default-btn"
+          className="h-40 xs:w-full lg:w-[630px]"
+          labelClass="text-default-btn xs:mb-10 lg:mb-16"
           labelSize="base"
           id="name"
           {...register('name')}
@@ -81,8 +83,8 @@ const RegistrationForm: React.FC = () => {
         <InputField
           label="Адреса електронної пошти"
           placeholder="Введіть адресу електронної пошти"
-          className="w-[630px] h-40"
-          labelClass="text-default-btn mb-16"
+          className="h-40 xs:w-full lg:w-[630px]"
+          labelClass="text-default-btn xs:mb-10 lg:mb-16"
           labelSize="base"
           id="email"
           {...register('email')}
@@ -90,11 +92,11 @@ const RegistrationForm: React.FC = () => {
             (activeDialog !== 'login' && emailError) || errors.email?.message
           }
         />
-        <div className="flex gap-20">
+        <div className="flex gap-20 xs:flex-col lg:flex-row">
           <div className="flex flex-col text-[16px]">
             <label
               htmlFor="location"
-              className="text-base text-left text-default-btn mb-16 leading-[125%]"
+              className="text-base text-left text-default-btn leading-[125%] xs:mb-10 lg:mb-16"
             >
               Місто
             </label>
@@ -105,7 +107,7 @@ const RegistrationForm: React.FC = () => {
                 <CitySelect
                   value={field.value}
                   onChange={field.onChange}
-                  className="w-[305px] h-40 text-[16px] hover:border-input-border"
+                  className="h-40 text-[16px] hover:border-input-border xs:w-full lg:w-[305px]"
                   errorMess={errors.location?.message}
                 />
               )}
@@ -114,8 +116,8 @@ const RegistrationForm: React.FC = () => {
           <PhoneInput
             label="Номер телефону"
             placeholder="+380"
-            className="w-[305px] h-40"
-            labelClass="text-default-btn mb-16"
+            className="h-40 xs:w-full lg:w-[305px]"
+            labelClass="text-default-btn xs:mb-10 lg:mb-16"
             labelSize="base"
             id="phone"
             {...register('phone')}
@@ -125,8 +127,9 @@ const RegistrationForm: React.FC = () => {
         <CustomRadioGroup
           groupLabel="Оберіть хто Ви"
           labelSize="base"
-          labelClass="mb-16 text-default-btn"
-          className="text-[16px] text-default-btn"
+          labelClass="text-default-btn xs:mb-10 lg:mb-16"
+          className="text-[16px] text-default-btn xs:flex-col xs:gap-10 sm:flex-row sm:gap-20"
+          itemWidth="xs:w-full"
           items={userTypeOptions}
           value={userTypeValue}
           onChange={value =>
@@ -138,8 +141,8 @@ const RegistrationForm: React.FC = () => {
         <PasswordField
           label="Пароль"
           placeholder="Введіть надійний пароль"
-          className="w-[630px] h-40"
-          labelClass="leading-[125%] mb-16 text-default-btn"
+          className="h-40 xs:w-fulll g:w-[630px]"
+          labelClass="leading-[125%] text-default-btn xs:mb-10 lg:mb-16"
           labelSize="base"
           id="password"
           {...register('password')}
@@ -151,8 +154,8 @@ const RegistrationForm: React.FC = () => {
         <PasswordField
           label="Повторіть пароль"
           placeholder="Введіть пароль повторно"
-          className="w-[630px] h-40"
-          labelClass="leading-[125%] mb-16 text-default-btn"
+          className="h-40 xs:w-full lg:w-[630px]"
+          labelClass="leading-[125%] text-default-btn xs:mb-10 lg:mb-16"
           labelSize="base"
           id="repeat_password"
           {...register('repeat_password')}
@@ -161,7 +164,7 @@ const RegistrationForm: React.FC = () => {
         <CustomButton
           type="submit"
           styleType="defaultButton"
-          className="mt-50 text-[16px]"
+          className="text-[16px] xs:mt-4 lg:mt-50"
         >
           Зареєструватися
         </CustomButton>
--- src/pages/Auth/Registration.tsx
+++ src/pages/Auth/Registration.tsx
@@ -12,23 +12,27 @@ const Registration = () => {
   return (
     <div className="container relative h-full">
       <div className="absolute top-[13px] right-[80px] w-[771px] h-[1078px] bg-[url('./src/assets/backgrounds/bg-paws-register.png')] bg-contain bg-no-repeat " />
-      <div className="grid grid-cols-2 gap-20 leading-[140%] tracking-[0.01em] pt-100 relative ">
-        <div>
-          <h2 className="text-[32px] mb-32">Ласкаво просимо</h2>
-          <p className="text-2xl text-left mb-50">
-            Щоб мати можливість швидко написати оголошення, або зберегти анкети
-            тваринок, які вам сподобалися, увійдіть
-          </p>
-          <CustomButton
-            styleType="defaultButton"
-            className="mb-50 mt-0"
-            onClick={() => dispatch(openDialog('login'))}
-          >
-            Зайти в особистий кабінет
-          </CustomButton>
+      <div className="grid leading-[140%] tracking-[0.01em] text-default-btn relative items-center text-center xs:grid-cols-1 xs:gap-32 xs:pt-40 xs:pb-80 lg:grid-cols-2 lg:gap-20 lg:pt-100 lg:pb-0">
+        <div className="flex xs:flex-col-reverse lg:flex-col">
+          <div>
+            <h2 className="xs:text-[18px] xs:mb-10 xs:mt-16 lg:text-[32px] lg:mb-32 lg:mt-0">
+              Ласкаво просимо
+            </h2>
+            <p className="xs:mb-20 xs:text-center xs:text-[16px] lg:mb-50 lg:text-left lg:text-2xl">
+              Щоб мати можливість швидко написати оголошення, або зберегти
+              анкети тваринок, які вам сподобалися, увійдіть
+            </p>
+            <CustomButton
+              styleType="defaultButton"
+              className="mt-0 text-[16px] xs:mb-0 lg:mb-50"
+              onClick={() => dispatch(openDialog('login'))}
+            >
+              Вхід в особистий кабінет
+            </CustomButton>
+          </div>
 
-          <div className="relative w-[630px] h-[571px] bg-orange rounded-[30px]">
-            <div className="absolute bottom-0 left-0 w-[598px] h-[539px] bg-main-pink-l rounded-[20px] overflow-hidden">
+          <div className="relative bg-orange rounded-[30px] m-auto xs:w-full xs:h-[210px] lg:w-[630px] lg:h-[571px]">
+            <div className="absolute bottom-0 left-0 bg-main-pink-l rounded-[20px] overflow-hidden xs:w-full xs:h-[194px] lg:w-[598px] lg:h-[539px]">
               <ResponsiveImage
                 urlMax={dogWithRaisedPawMax}
                 urlMin={dogWithRaisedPawMin}
