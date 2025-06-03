e619bf8ee15f2041e2b4d7bb60b5db3b89738561
MilaKoseckaya <kosetsaya@gmail.com>
2025-06-03 20:35:36 +0300

    feat: create BlogCard

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+const BlogCard = ({
+  image,
+  title,
+  link,
+}: {
+  image: string;
+  title: string;
+  link: string;
+}) => {
+  return (
+    <div className="flex flex-col text-left">
+      <Link to={link} className="block mb-4">
+        <img
+          src={image}
+          alt={title}
+          className="mb-16 rounded-4xl w-full h-[280px] object-cover"
+        />
+      </Link>
+      <h3 className="font-medium text-base">{title}</h3>
+      <Link to={link} className="text-link text-sm underline">
+        Читати більше
+      </Link>
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/pages/Blog/BlogMain.tsx
+++ src/pages/Blog/BlogMain.tsx
@@ -1,5 +1,17 @@
+import BlogCard from 'components/BlogCard';
+
 const BlogMain = () => {
-  return <div>BlogMain</div>;
+  return (
+    <div className="container">
+      <div className="gap-4 grid grid-cols-3">
+        <BlogCard
+          title="Знайдіть свого ідеального компаньйона: Поради щодо вибору тварини"
+          image="/blog/blog-1.jpg"
+          link="/blog/1"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BlogMain;
