09fd374690c0f2bee57aaae4c4e92c582e7f86e6
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-06-05 13:02:03 +0300

    fix(TM-257): Animal card is not removed from favorites or viewing history after deletion

--- src/hooks/useSyncFavoritesOnLogin.ts
+++ src/hooks/useSyncFavoritesOnLogin.ts
@@ -1,13 +1,21 @@
 import { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { useAddFavoriteAnimalMutation } from '../redux/animals/animalsApi';
-import { selectFavoriteIds } from '../redux/animals/favoriteAnimalsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  useAddFavoriteAnimalMutation,
+  useLazyGetAnimalByIdQuery,
+} from '../redux/animals/animalsApi';
+import {
+  removeAnimal,
+  selectFavoriteIds,
+} from '../redux/animals/favoriteAnimalsSlice';
 import { selectIsLoggedIn } from '../redux/users/usersSlice';
 
 export const useSyncFavoritesOnLogin = () => {
   const favoriteIds = useSelector(selectFavoriteIds);
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const [addFavorite] = useAddFavoriteAnimalMutation();
+  const [triggerGetAnimalById] = useLazyGetAnimalByIdQuery();
+  const dispatch = useDispatch();
 
   const hasSyncedRef = useRef(false);
   const wasLoggedInRef = useRef(false);
@@ -15,17 +23,44 @@ export const useSyncFavoritesOnLogin = () => {
   useEffect(() => {
     if (!wasLoggedInRef.current && isLoggedIn && !hasSyncedRef.current) {
       (async () => {
+        const validIds: string[] = [];
+
         for (const id of favoriteIds) {
           try {
+            const res = await triggerGetAnimalById(id).unwrap();
+
+            if (
+              !res ||
+              res.animal.status === 'inactive' ||
+              res.animal.isHidden
+            ) {
+              throw new Error('Not found or hidden');
+            }
+
             await addFavorite(id).unwrap();
+            validIds.push(id);
           } catch (error) {
-            console.error(error);
+            if (
+              typeof error === 'object' &&
+              error !== null &&
+              'status' in error &&
+              error.status === 404
+            ) {
+              dispatch(removeAnimal(id));
+            }
           }
         }
+
+        if (validIds.length > 0) {
+          localStorage.setItem('favorites', JSON.stringify(validIds));
+        } else {
+          localStorage.removeItem('favorites');
+        }
+
         hasSyncedRef.current = true;
       })();
     }
 
     wasLoggedInRef.current = isLoggedIn;
-  }, [isLoggedIn, favoriteIds, addFavorite]);
+  }, [isLoggedIn, favoriteIds, addFavorite, triggerGetAnimalById, dispatch]);
 };
