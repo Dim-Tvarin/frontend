ae609f5bea4e58213cc84504864a42b647855813
MilaKoseckaya <kosetsaya@gmail.com>
2025-06-06 14:28:17 +0300

    fix: edit filter labels

--- src/components/FilterItem.tsx
+++ src/components/FilterItem.tsx
@@ -46,7 +46,7 @@ const FilterItem = ({
           <FaChevronDown color="#042D4A" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="bg-white shadow-none p-4 border-1 border-input-border w-(--radix-popover-trigger-width) text-default-btn">
+      <PopoverContent className="bg-white shadow-none p-4 border-1 border-input-border w-(--radix-popover-trigger-width) text-default-btn -mt-4">
         <RadioGroup value={value} onValueChange={handleValueChange}>
           {items.map(item => (
             <div
