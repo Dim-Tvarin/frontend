d85c38b2871433071245ee480a94cd30bd325d5a
MilaKoseckaya <kosetsaya@gmail.com>
2025-06-13 19:40:19 +0300

    fix: footer

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
     }
   }, [theme]);
   useSyncFavoritesOnLogin();
-  console.log('week-19');
+  console.log('week-19.1');
   return (
     <Routes>
       <Route path="/" element={<Main />}>
