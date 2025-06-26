f19addef11e94a5a1ba02764ecd054bcf246cb35
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-06-26 11:18:46 +0300

    fix(TM-298): outline in close btn

--- src/assets/CloseSVG.tsx
+++ src/assets/CloseSVG.tsx
@@ -8,12 +8,13 @@ const CloseSVG = ({
   size?: string;
 }): ReactElement => {
   return (
-    <div className="w-32 h-32 flex items-center justify-center">
+    <div className="w-32 h-32 flex items-center justify-center focus-visible:outline-none">
       <svg
         width={size || '24'}
         height={size || '24'}
         viewBox="0 0 24 24"
         fill="none"
+        className="focus:outline-none"
       >
         <path
           d="M12 22.1477C17.5228 22.1477 22 17.6405 22 12.0805C22 6.52063 17.5228 2.01343 12 2.01343C6.47715 2.01343 2 6.52063 2 12.0805C2 17.6405 6.47715 22.1477 12 22.1477Z"
@@ -21,6 +22,7 @@ const CloseSVG = ({
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
+          className="focus:outline-none"
         />
         <path
           d="M9.17163 9.23312L14.8285 14.9279"
