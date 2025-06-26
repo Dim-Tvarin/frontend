f7d51be38a885fb600ffe11d34da65b3e72c07b8
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-06-26 23:07:59 +0300

    feat: add sorting and pagination to favorites page

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -2,7 +2,7 @@ import AnimalCard from 'components/AnimalCard';
 import Pagination from 'components/Pagination';
 import ResponsiveImage from 'components/ResponsiveImage';
 import { PetsListSkeleton } from 'components/sceletons/PetsListSkeleton';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useSearchParams } from 'react-router';
 import {
@@ -12,38 +12,113 @@ import {
 import emptyFavoriteMax from '../assets/empty-favorites@2x.png';
 import emptyFavoriteMin from '../assets/empty-favorites@1x.png';
 import { CustomButton } from 'components/CustomButton';
+import { cn } from 'components/lib/utils';
+
+type SortOrderFavorites = 'newest' | 'oldest' | 'none';
 
 const FavoritePage = () => {
   const navigate = useNavigate();
   const isLoading = useSelector(selectFavoritesLoading);
   const favorites = useSelector(selectFavoriteAnimals);
-  const perPage = 16;
+  const perPage = 12;
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const rawPage = Number(searchParams.get('page')) || 1;
   const [page, setPage] = useState(rawPage);
 
-  const totalPages = Math.ceil(favorites.length / perPage);
-  const paginatedFavorites = favorites.slice(
-    (page - 1) * perPage,
-    page * perPage
-  );
+  const [sorting, setSorting] = useState<SortOrderFavorites>('none');
+  const [openSorting, setOpenSorting] = useState(false);
 
+  const sortedFavorites = useMemo(() => {
+    if (sorting === 'none') return favorites;
+    return [...favorites].sort((a, b) => {
+      const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
+      const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
+      if (sorting === 'newest') return dateB - dateA;
+      if (sorting === 'oldest') return dateA - dateB;
+      return 0;
+    });
+  }, [favorites, sorting]);
+
+  const totalPages = Math.ceil(sortedFavorites.length / perPage);
+  const paginatedFavorites = useMemo(
+    () => sortedFavorites.slice((page - 1) * perPage, page * perPage),
+    [sortedFavorites, page]
+  );
+  console.log(paginatedFavorites);
   useEffect(() => {
     if (rawPage > 0 && rawPage !== page) {
       setPage(rawPage);
     }
-  }, [rawPage]);
+  }, [rawPage, page]);
 
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
-    setSearchParams({ page: String(newPage) });
+  const handleAscSorting = () => {
+    setSorting('newest');
+    setOpenSorting(false);
+    setPage(1);
+  };
+  const handleDescSorting = () => {
+    setSorting('oldest');
+    setOpenSorting(false);
+    setPage(1);
+  };
+  const handleClearFilter = () => {
+    setSorting('none');
+    setOpenSorting(false);
+    setPage(1);
   };
 
   return (
     <div className="container flex flex-col h-full grow">
-      <div className="relative text-center mt-100 mb-50">
-        <h1 className="text-[32px] text-default-btn w-full">Обрані</h1>
+      <div className="relative mt-100 mb-50">
+        <div className="flex">
+          <h1 className="text-[32px] text-center text-default-btn w-full">
+            Обрані
+          </h1>
+          <div className="absolute z-10 flex flex-col right-0">
+            <CustomButton
+              type="button"
+              styleType="whiteButton"
+              className="m-0 w-[192px] md:w-[227px] text-default-btn text-medium text-base"
+              onClick={() => setOpenSorting(prev => !prev)}
+            >
+              {sorting === 'none' && 'Сортування за датою'}
+              {sorting === 'newest' && 'Останні оголошення'}
+              {sorting === 'oldest' && 'Давні оголошення'}
+            </CustomButton>
+            {openSorting && (
+              <div className="w-[192px] md:w-[227px] flex flex-col gap-4 bg-dialog px-16 py-10 border-1 border-default-btn rounded-xl">
+                <button
+                  onClick={handleAscSorting}
+                  className={cn(
+                    'text-default-btn text-left text-lg focus:outline-none hover:text-orange transition-all duration-300',
+                    sorting === 'newest' && 'text-orange'
+                  )}
+                >
+                  Останні оголошення
+                </button>
+                <button
+                  onClick={handleDescSorting}
+                  className={cn(
+                    'text-default-btn text-left text-lg focus:outline-none hover:text-orange transition-all duration-300',
+                    sorting === 'oldest' && 'text-orange'
+                  )}
+                >
+                  Давні оголошення
+                </button>
+                <button
+                  onClick={handleClearFilter}
+                  className={cn(
+                    'text-default-btn text-left text-lg focus:outline-none hover:text-orange transition-all duration-300',
+                    sorting === 'none' && 'text-orange'
+                  )}
+                >
+                  Очистити фільтр
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
         {favorites.length === 0 && (
           <div className="flex flex-col align-center justify-center text-lg text-default-btn">
             <div className="w-[332px] h-[375px] mt-40 self-center">
@@ -70,8 +145,8 @@ const FavoritePage = () => {
         <PetsListSkeleton />
       ) : (
         <>
-          <div className="flex gap-20 flex-col h-full grow">
-            <div className="grid gap-20 wrap transition-all duration-500 grid-cols-4 ">
+          <div className="flex gap-50 flex-col grow">
+            <div className="grid gap-20 wrap transition-all duration-500 grid-cols-4">
               {paginatedFavorites.map(item => (
                 <AnimalCard
                   key={item.id}
@@ -85,15 +160,14 @@ const FavoritePage = () => {
                 />
               ))}
             </div>
-            {totalPages > 1 && (
-              <div className="mt-auto">
-                <Pagination
-                  currentPage={page}
-                  totalPages={totalPages}
-                  onPageChange={handlePageChange}
-                  className="mb-100"
-                />
-              </div>
+
+            {favorites && !!totalPages && totalPages > 1 && (
+              <Pagination
+                onPageChange={setPage}
+                currentPage={page}
+                totalPages={totalPages}
+                className="mb-100 mt-auto"
+              />
             )}
           </div>
         </>
