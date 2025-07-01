baaf865362793815210861a7b4dc0d6b946035ad
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-07-01 14:24:06 +0300

    fix: public path terminal errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import homeDogMin from '../assets/home-dog1.jpg';
 import homeDogMax from '../assets/home-dog2.png';
 import homeGirlDogMin from '../assets/home-girl&dog1.jpg';
 import homeGirlDogMax from '../assets/home-girl&dog@2.jpg';
-import track from '../../public/track.png';
+import track from '/track.png?url';
 import { LuCirclePlus } from 'react-icons/lu';
 import AnimalsCarousel from 'components/AnimalsCarousel';
 import { useSelector } from 'react-redux';
