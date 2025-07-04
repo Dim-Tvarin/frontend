0be48e0ed2bd0fbe72b88b3de7b4c766a56c12f7
MilaKoseckaya <kosetsaya@gmail.com>
2025-07-04 17:44:57 +0300

    fix: hart stroke

--- src/assets/HartSVG.tsx
+++ src/assets/HartSVG.tsx
@@ -28,10 +28,8 @@ const HartSVG = ({
       />
       <path
         d="M18.62 26.8101C18.28 26.9301 17.72 26.9301 17.38 26.8101C14.48 25.8201 8 21.6901 8 14.6901C8 11.6001 10.49 9.1001 13.56 9.1001C15.38 9.1001 16.99 9.9801 18 11.3401C19.01 9.9801 20.63 9.1001 22.44 9.1001C25.51 9.1001 28 11.6001 28 14.6901C28 21.6901 21.52 25.8201 18.62 26.8101Z"
-        className={`
-        stroke-[${hartFill ? '#EB5050' : stroke}]
-        dark:stroke-header
-      `}
+        className={`dark:stroke-header`}
+        stroke={hartFill ? '#EB5050' : stroke}
         fill={hartFill ? '#EB5050 ' : 'none'}
         strokeWidth="1.5"
         strokeLinecap="round"
--- src/pages/PetPage.tsx
+++ src/pages/PetPage.tsx
@@ -122,7 +122,7 @@ const PetPage = () => {
       console.error('Не вдалося змінити видимість:', error);
     }
   };
-  
+  console.log('isInFavorites', isInFavorites);
   return (
     <div className="container">
       <div className="relative flex lg:flex-row flex-col gap-20 mt-72 lg:mt-100 mb-100 text-default-btn">
@@ -133,7 +133,7 @@ const PetPage = () => {
           {animal && <ImageCarousel images={animal?.animalImages} />}
         </div>
 
-        <div className="z-10 flex flex-col w-full lg:w-1/2 text-lg lg:text-xl text-left">
+        <div className="z-10 flex flex-col w-full lg:w-1/2 text-lg lg:text-base text-left">
           <div className="flex items-center">
             <h2 className="mb-16 lg:mb-24 font-bold text-2xl md:text-4xl lg:text-5xl mr-100">
               {animal?.animalName}
@@ -156,7 +156,7 @@ const PetPage = () => {
           <div className="text-input-border text-sm mb-6">
             Опубліковано: {animal?.updatedAt && formatDate(animal?.updatedAt)}
           </div>
-          <div className="gap-x-auto gap-y-16 grid grid-cols-2 mb-16 lg:mb-32">
+          <div className="gap-x-auto gap-y-10 lg:gap-y-16 grid grid-cols-2 mb-16 lg:mb-32">
             <p className="font-bold">Статус:</p>
             <p>
               {animal?.status === 'active'
