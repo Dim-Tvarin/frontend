313f481b2755225b95891ca8ebd4b355c1aeeed0
Mila <85253058+koseckaya@users.noreply.github.com>
2025-07-30 18:28:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
     }
   }, [theme]);
   useSyncFavoritesOnLogin();
-  console.log('week-24');
+  console.log('week-25');
   return (
     <Routes>
       <Route path="/" element={<Main />}>
